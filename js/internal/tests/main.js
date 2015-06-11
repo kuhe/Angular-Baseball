55b55ddaefac4265971ad77342a7e6508619d531
George Fu <gjf@foundationcenter.org>
2015-06-11 14:26:33 -0400

    testing for fake stat consistency with actual performance

--- js/internal/tests/main.js
+++ js/internal/tests/main.js
@@ -27,19 +27,45 @@ log = function() {
     console.log.apply(console, arguments);
 };
 
+stat = function(n) {
+    return Math.floor(n*1000)/1000;
+};
+
 Game.prototype.console = true;
 Game.prototype.humanControl = 'none';
 Game.prototype.quickMode = true;
 Animator.console = true;
 
 game = new Game();
-var n = 0;
+game.gamesIntoSeason = 80;
+p = player = new Player(game.teams.home);
+game.teams.home.lineup = [p,p,p,p,p,p,p,p,p];
+
+logPlayer = function() {
+    log(
+        'PA ' + stat(player.stats.batting.pa),
+        'AV ' + stat(player.stats.batting.getBA()),
+        'H '  + stat(player.stats.batting.h),
+        '2B ' + stat(player.stats.batting['2b']),
+        '3B ' + stat(player.stats.batting['3b']),
+        'HR ' + stat(player.stats.batting.hr)
+    );
+};
+
+logPlayer();
+var pa = player.stats.batting.pa;
 do {
-    n++;
     game.simulateInput(function(callback) {
         typeof callback == 'function' && callback();
     });
-} while (game.stage != 'end' && n < 3000);
-game.debugOut();
+    if (game.stage == 'end') {
+        game.inning = 1;
+        game.half = 'top';
+        game.stage = 'pitch';
+    }
+} while (player.stats.batting.pa < pa * 2);
 
+// game.debugOut();
 //log('last contact result', game.debug[game.debug.length - 1]);
+
+logPlayer();
