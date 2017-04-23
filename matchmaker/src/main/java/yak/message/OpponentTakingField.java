c48c3633229122bbd9b815dd4effd6c126d61936
kuhe <george.jing.fu@gmail.com>
2017-04-23 03:14:11 -0400

    remove todo

--- matchmaker/src/main/java/yak/message/OpponentTakingField.java
+++ matchmaker/src/main/java/yak/message/OpponentTakingField.java
@@ -1,11 +1,8 @@
 package yak.message;
 
-import yak.annotation.Todo;
-
 /**
  * A signal to send the game data to the opponent.
  */
-@Todo
 public class OpponentTakingField extends Message {
 
     public final String type = "opponent_taking_field";
