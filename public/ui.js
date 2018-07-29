bc0e980b0dea4aa0e859b9a4b5f16253cd7fd5d0
kuhe <george.jing.fu@gmail.com>
2018-07-29 18:30:38 -0400

    wip

---
+++ public/ui.js
@@ -0,0 +1,15 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof define === 'function' && define.amd ? define(['exports'], factory) :
+    (factory((global.yakyuu = {})));
+}(this, (function (exports) { 'use strict';
+
+    function main() {
+        return 0;
+    }
+
+    exports.main = main;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
